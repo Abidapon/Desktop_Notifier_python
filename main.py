335adad54ce8d8c34ec35f5211c144647a1d157e
rcss <aponabid154@gmail.com>
2023-10-10 00:54:12 +0600

    Code Up

---
+++ main.py
@@ -0,0 +1,5 @@
+from win10toast import ToastNotifier
+Toast = ToastNotifier()
+Toast.show_toast("Learning Time", "Hey Abid, Time to learn something new!", duration=3 )
+Toast.show_toast("Reminder", "Hey Abid, Time to learn something new!", duration=5 )
+Toast.show_toast("Again Reminder", "Hey Abid, Time to learn something new!", duration=7 )
